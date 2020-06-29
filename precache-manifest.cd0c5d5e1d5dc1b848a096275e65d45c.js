451b9cbf90dc21fc13708f455794233165c20f1e
shivang02 <shivang.dubey1@student.amity.edu>
2020-06-30 00:31:05 +0530

    Updates

---
+++ precache-manifest.cd0c5d5e1d5dc1b848a096275e65d45c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d0583879519c96cefa8044513c70ae",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "31f7de4a9030099105b7",
+    "url": "/to-do-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34b3fc667e5d83bbb791",
+    "url": "/to-do-list/static/js/2.16b6be71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.16b6be71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f7de4a9030099105b7",
+    "url": "/to-do-list/static/js/main.d784f5e6.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
