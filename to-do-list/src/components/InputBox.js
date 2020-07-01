e0a33991d43e9c8972e00cb8dedfe50713525afc
shivang02 <shivang.dubey1@student.amity.edu>
2020-07-01 17:46:31 +0530

    Added some major changes

--- to-do-list/src/components/InputBox.js
+++ to-do-list/src/components/InputBox.js
@@ -7,24 +7,38 @@ class InputBox extends React.Component {
         this.state ={
             ItemVal : []
         }
+        this.ClickHandler = this.ClickHandler.bind(this);
+        this.EnterCheck=this.EnterCheck.bind(this);
     }
     
-    ClickHandler = () => {
-        console.log(this.state.ItemVal);
-        return (this.state.ItemVal).push([document.getElementById("input-box").value]);
-        
-    };
     
+    ClickHandler = () => {
+        const str=document.getElementById("input-box").value;
+        this.setState({ItemVal:this.state.ItemVal.concat(str)});
+        document.getElementById("input-box").value="";
+    };
+
+    EnterCheck = (e)=>{
+        if(e.key== 'Enter'){
+            this.ClickHandler();
+        }    
+    }
     
     render() {
+        
+        const listItems = this.state.ItemVal.map((listVal,index) =>{
+            return (
+                <List value={listVal} />
+            );
+        })
         return (
             <div>
             <div className="input-container">
-            <input className="list-input" placeholder="Enter List Item" id="input-box"></input>
-                <button type="submit" className="list-submit-button" onClick={this.ClickHandler}>Add Note</button>
+            <input className="list-input" placeholder="Enter List Item" id="input-box" onKeyDown={this.EnterCheck}></input>
+                <button type="submit" id="submit-button" className="list-submit-button" onClick={() =>this.ClickHandler()}>Add Note</button>
             </div>
             <div className="full-list">
-                <List value={"hello12"} />
+                {listItems}
             </div>
         </div>
         )
